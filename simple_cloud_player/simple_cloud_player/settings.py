74612f44e149a78f0cf0a99e018ca53ecca70c5f
NingAnMe <ninganme@qq.com>
2018-09-15 22:16:21 +0800

    complete video model

--- simple_cloud_player/simple_cloud_player/settings.py
+++ simple_cloud_player/simple_cloud_player/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['95.169.24.76']
 # Application definition
 
 INSTALLED_APPS = [
+    'video.apps.VideoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
