6bd3539fe41cf2817707f8c91701a3fac745af9b
NingAnMe <ninganme@qq.com>
2018-09-15 22:50:19 +0800

    add video view: detail and play

--- simple_cloud_player/video/urls.py
+++ simple_cloud_player/video/urls.py
@@ -9,5 +9,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /video/
     path('', views.index, name='index'),
+    # ex: /video/5/
+    path('<int:video_id>/', views.detail, name='detail'),
+    # ex: /video/5/play/
+    path('<int:video_id>/play/', views.play, name='results'),
 ]
--- simple_cloud_player/video/views.py
+++ simple_cloud_player/video/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, video_id):
+    return HttpResponse("You're looking at video %s." % video_id)
+
+
+def play(request, video_id):
+    return HttpResponse("You're looking at play %s." % video_id)
